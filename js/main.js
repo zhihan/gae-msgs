19c3596b110cec4c98d1a1e10033ff318922f6e1
Zhi Han <zhi.han@gmail.com>
2016-10-16 12:42:33 -0400

    Added get message.

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// Main entry for the javascript classes.
+
+(function(){
+    var msgs = [];
+    var init = function() {
+        var loginBtn = document.getElementById('btn-login');
+        loginBtn.onclick = messages.signIn;
+
+        var logoutBtn = document.getElementById('btn-logout');
+        logoutBtn.onclick = messages.signOut;
+
+        var refreshBtn = document.getElementById('btn-refresh');
+        refreshBtn.onclick = refreshMessages;
+    }
+
+    var updateMessages = function(result) {
+        if (result.receivedMessages) {
+            result.receivedMessages.forEach(function(message) {
+                msgs.push(atob(message.message.data));
+            });
+        }
+
+        var txt = document.getElementById('messages');
+        txt.innerHTML = msgs.join('<br>');
+    }
+
+    var refreshMessages = function() {
+        messages.getMessage('projects/msgs-sample/subscriptions/first-sub',
+                            updateMessages);
+    }
+    
+   document.addEventListener('DOMContentLoaded', init, false);
+
+})();
--- js/messages.js
+++ js/messages.js
@@ -1,44 +1,87 @@
 /** Messages using cloud pub/sub */
 
 var messages = (function(){
-	var clientId = '388468322983-72fd13eu4l5vad93ljef5a71s5ujpbch.apps.googleusercontent.com';
-	var clientSecret = '5J4hsHo4paN8MRQZdLyRBrBw';
-	var pubsubScopes = ['https://www.googleapis.com/auth/pubsub',
-						'https://www.googleapis.com/auth/cloud-platform'].join(' ');
-	var apiKey = 'AIzaSyBm2-1AaEqgq4UkGtZXt__3XGfSiBSA2-0';
+    var clientId = '388468322983-72fd13eu4l5vad93ljef5a71s5ujpbch.apps.googleusercontent.com';
+    var clientSecret = '5J4hsHo4paN8MRQZdLyRBrBw';
+    var pubsubScopes = ['https://www.googleapis.com/auth/pubsub',
+						            'https://www.googleapis.com/auth/cloud-platform'].join(' ');
+	  var apiKey = 'AIzaSyBm2-1AaEqgq4UkGtZXt__3XGfSiBSA2-0';
+    
+	  /** Initialize the module with auth2 and pubsub API. */
+	  var init = function() {
+		    gapi.client.setApiKey(apiKey);
+		    gapi.auth2.init({
+			      client_id: clientId,
+			      scope: pubsubScopes
+		    });
+        gapi.client.load('pubsub');
+	  };
+	  
+    /** List the number of subscriptions. */
+	  var listSubscriptions = function() {
+		    gapi.client.pubsub.projects.subscriptions.list(
+			      {"project": "projects/msgs-sample/topics/my-first-topic"}
+		    ).then(function(resp) {
+            console.log(resp);
+        });		
+	  };
 
-	/** Initialize the module with auth2 and pubsub API. */
-	var init = function() {
-		gapi.client.setApiKey(apiKey);
-		gapi.auth2.init({
-			client_id: clientId,
-			scope: pubsubScopes
-		}).then(function() {
-			gapi.auth2.getAuthInstance().isSignedIn.listen(initPubsub)
-			
-			if (gapi.auth2.getAuthInstance().isSignedIn.get()) {
-				initPubsub();
-			}
-			gapi.auth2.getAuthInstance().signIn();
-		});
-	};
+    /** */
+    var logMessages = function(subscription) {
+        gapi.client.pubsub.projects.subscriptions.pull(
+            {'subscription': subscription,
+             'returnImmediately': true,
+             'maxMessages': 1}
+        ).then(function(resp) { console.log(resp); });
+            
+    };
 
-	var initPubsub = function() {
-		gapi.client.load('pubsub').then(
-			listSubscriptions
-		);
-	};
-	
+    var checkedCallback = function(callback, resp) {
+        if (resp.status == 200) {
+            callback(resp.result);
+        } else {
+            console.log('Error');
+        }
+    };
 
-	var listSubscriptions = function() {
-		gapi.client.pubsub.projects.subscriptions.list(
-			{"project": "projects/msgs-sample/topics/my-first-topic"}
-		).then(function(resp) { console.log(resp); })		
-	};
-	
-	return {
-		init: init
-	};
+    var checkResponse = function(resp) {
+        if (resp.status != 200) {
+            console.log('Error ' + resp);
+        }
+    }
+
+    /** Acknoledge receiption of the messages. */
+    var acknowledgeIfNecessary = function(subscription, result) {
+        if (result.receivedMessages) {
+            result.receivedMessages.forEach(function(message) {
+                gapi.client.pubsub.projects.subscriptions.acknowledge(
+                    {'subscription': subscription,
+                     'ackIds': [message.ackId]}
+                ).then(checkResponse);
+            });
+        }
+    };
+    
+    var getMessage = function(subscription, callback) {
+        gapi.client.pubsub.projects.subscriptions.pull(
+            {'subscription': subscription,
+             'returnImmediately': true,
+             'maxMessages': 1}).then(function(resp) {
+                 checkedCallback(
+                     function(result){
+                         acknowledgeIfNecessary(subscription, result);
+                         callback(result);
+                     }, resp)
+             });
+    };
+    
+	  return {
+		    init: init,
+        signIn: function(){ gapi.auth2.getAuthInstance().signIn(); },
+        signOut: function(){ gapi.auth2.getAuthInstance().signOut(); },
+        logMessages: logMessages,
+        getMessage: getMessage
+	  };
 })();
 
 
